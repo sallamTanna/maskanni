92297045f6bdc57bbcbf024f38784bcc0f236d12
unknown <sallamtanna2015@hotmail.com>
2020-01-21 16:14:30 +0200

    finish navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Layout, Menu } from "antd";
+import { Link } from "react-router-dom";
 
-import logo from "../../assets/logo1.png";
+import Button from "../Button";
+import logo from "../../assets/navbar-logo.png";
 
 import "./style.css";
 
@@ -9,18 +11,19 @@ const { Header } = Layout;
 
 class Navbar extends React.Component {
   render() {
+    const { history } = this.props;
     return (
-      <>
-        <Header style={{ backgroundColor: "white" }}>
+      <Header style={{ backgroundColor: "white", paddingLeft: 0 }} className="Navbar">
+        <div className="Navbar__menu">
           <Menu
-            className="test"
+            className="Navbar__item"
             theme="#fff"
             mode="horizontal"
-            defaultSelectedKeys={["1"]}
+            defaultSelectedKeys={["2"]}
             style={{ lineHeight: "64px" }}
           >
-            <Menu.Item key="1">
-              <img src={logo} width="10%" height="5%" alt="logo" />
+            <Menu.Item key="1" disabled>
+              <img src={logo} alt="logo" />
             </Menu.Item>
             <Menu.Item key="2">الرئيسية</Menu.Item>
             <Menu.Item key="3">المشاريع</Menu.Item>
@@ -29,8 +32,21 @@ class Navbar extends React.Component {
             <Menu.Item key="6">من نحن</Menu.Item>
             <Menu.Item key="7">اتصل بنا</Menu.Item>
           </Menu>
-        </Header>
-      </>
+        </div>
+        <div className="Navbar__links">
+          <Link>طلب تصميم خاص</Link>
+          <Button
+            label="تسجيل الدخول"
+            className="Navbar__links-login"
+            onClick={() => history.push("/login")}
+          />
+          <Button
+            label="تسجيل حساب جديد"
+            className="Navbar__links-signup"
+            onClick={() => history.push("/signup")}
+          />
+        </div>
+      </Header>
     );
   }
 }
