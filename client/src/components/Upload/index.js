614dc678fb5abd92f6cfe80e4a33b88978db73f9
SallamTanna <sallamtanna2015@hotmail.com>
2020-01-24 01:39:56 +0200

    remove logs

--- client/src/components/Upload/index.js
+++ client/src/components/Upload/index.js
@@ -4,8 +4,6 @@ import { Upload, Icon, Modal } from "antd";
 import "./style.css";
 
 function getBase64(file) {
-  console.log("fileeeeeeeee", file);
-
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
