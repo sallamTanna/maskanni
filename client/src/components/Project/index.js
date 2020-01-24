179c38d1515334d3acc85f54f6e987afb8115920
unknown <sallamtanna2015@hotmail.com>
2020-01-24 20:07:52 +0200

    make the project component responsive

--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -4,31 +4,40 @@ import "./style.css";
 
 class Project extends React.Component {
   render() {
-    const { src, name, roomsNumber, livingRoomsNumber, floorsNumber, totalSize } = this.props;
+    const {
+      src,
+      name,
+      roomsNumber,
+      livingRoomsNumber,
+      floorsNumber,
+      totalSize,
+      handleSetting,
+      handleFavorite,
+    } = this.props;
     return (
       <div className="project">
         <div className="project__icons">
-          <i className="fa fa-ellipsis-h" />
-          <i className="fa fa-heart" />
+          <i className="fa fa-ellipsis-h" onClick={handleSetting} />
+          <i className="fa fa-heart" onClick={handleFavorite} />
         </div>
         <div className="project__image">
           <img src={src} alt="projectImage" />
         </div>
         <p className="project__name">{name}</p>
         <div className="project_summary">
-          <div>
+          <div className="data1">
             <p>{roomsNumber}</p>
             <p>الغرف</p>
           </div>
-          <div>
+          <div className="data2">
             <p>{livingRoomsNumber}</p>
             <p>غرف المعيشة</p>
           </div>
-          <div>
+          <div className="data3">
             <p>{floorsNumber}</p>
             <p>الادوار</p>
           </div>
-          <div>
+          <div className="data4">
             <p>{totalSize}</p>
             <p>المساحة الكلية</p>
           </div>
