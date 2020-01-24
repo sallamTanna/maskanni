b166feaf6534c2ac7dabb414d0e40642b1518acd
unknown <sallamtanna2015@hotmail.com>
2020-01-24 21:57:17 +0200

    add projects page

--- client/src/pages/Projects/index.js
+++ client/src/pages/Projects/index.js
@@ -3,6 +3,11 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import Header from "../../components/Header";
 import Project from "../../components/Project";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import InputNumber from "../../components/InputNumber";
+import CheckBox from "../../components/CheckBox";
+import Footer from "../../components/Footer";
 import test from "../../assets/test.png";
 
 import "./style.css";
@@ -64,8 +69,46 @@ class Projects extends React.Component {
               totalSize={5}
             />
           </div>
-          <div className="projects_filter">2</div>
+          <div className="projects_filter">
+            <p className="projects_filter__title">ترتيب النتائج</p>
+            <p>مساحة البناء</p>
+            <div className="size-filter">
+              <Input placeholder="الأقل" />
+              <Input placeholder="الأكثر" />
+              <Button label="بحث" />
+            </div>
+            <p>طول الواجهة</p>
+            <div className="length-filter">
+              <Input placeholder="الأقل" />
+              <Input placeholder="الأكثر" />
+              <Button label="بحث" />
+            </div>
+            <p>الإرتفاع</p>
+            <div className="hight-filter">
+              <Input placeholder="الأقل" />
+              <Input placeholder="الأكثر" />
+              <Button label="بحث" />
+            </div>
+            <p className="projects_filter__title">نوع التصميم / المخطط</p>
+            <div className="type-filter">
+              <CheckBox label="كل الأنواع" />
+              <CheckBox label="شقة ستوديو" />
+              <CheckBox label="شاليه" />
+              <CheckBox label="فيلا سكنية" />
+              <CheckBox label="مبنى كبير" />
+            </div>
+            <p className="projects_filter__title">عدد الأدوار</p>
+            <InputNumber symbol="أدوار" className="numbe-filter" />
+            <p className="projects_filter__title">عدد الغرف</p>
+            <InputNumber symbol="أدوار" className="numbe-filter" />
+            <p className="projects_filter__title">عدد غرف المعيشة</p>
+            <InputNumber symbol="أدوار" className="numbe-filter" />
+            <p className="projects_filter__title">عدد الحمامات</p>
+            <InputNumber symbol="أدوار" className="numbe-filter" />
+            <Button label="بحث" className="filter_search" />
+          </div>
         </div>
+        <Footer />
       </div>
     );
   }
