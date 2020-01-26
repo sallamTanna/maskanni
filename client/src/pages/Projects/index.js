dad3810ed9de1ebdd435e8dafd1c0cdaff7d3464
unknown <sallamtanna2015@hotmail.com>
2020-01-26 19:50:20 +0200

    add size, length, and height filters

--- client/src/pages/Projects/index.js
+++ client/src/pages/Projects/index.js
@@ -19,17 +19,33 @@ class Projects extends React.Component {
   state = {
     isLoading: true,
     projects: [],
+    allProjects: [],
     errors: false,
     errorMsg: "",
+    sizeMin: "",
+    sizeMax: "",
+    lengthMin: "",
+    lengthMax: "",
+    heightMin: "",
+    heightMax: "",
+    isEmptyProjects: false,
   };
 
   componentDidMount() {
     axios(`/v1/projects/`)
       .then(response => {
-        this.setState({
-          isLoading: false,
-          projects: response.data.response.data,
-        });
+        console.log(11111, response.data.response.data);
+        if (response.data.response.data.length === 0) {
+          this.setState({
+            isEmptyProjects: true,
+          });
+        } else {
+          this.setState({
+            isLoading: false,
+            projects: response.data.response.data,
+            allProjects: response.data.response.data,
+          });
+        }
       })
       .catch(() => {
         this.setState({
@@ -39,8 +55,56 @@ class Projects extends React.Component {
       });
   }
 
+  handleInputChange = e => {
+    const { allProjects } = this.state;
+    this.setState({
+      project: allProjects,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSizeFilter = () => {
+    const { allProjects, sizeMin, sizeMax } = this.state;
+    const filteredProjects = allProjects.filter(project => {
+      return project.size >= sizeMin && project.size <= sizeMax;
+    });
+    this.setState({
+      projects: filteredProjects,
+    });
+  };
+
+  handleLengthFilter = () => {
+    const { allProjects, lengthMin, lengthMax } = this.state;
+    const filteredProjects = allProjects.filter(project => {
+      return project.length >= lengthMin && project.length <= lengthMax;
+    });
+    this.setState({
+      projects: filteredProjects,
+    });
+  };
+
+  handleHeightFilter = () => {
+    const { allProjects, heightMin, heightMax } = this.state;
+    const filteredProjects = allProjects.filter(project => {
+      return project.height >= heightMin && project.height <= heightMax;
+    });
+    this.setState({
+      projects: filteredProjects,
+    });
+  };
+
   render() {
-    const { isLoading, projects } = this.state;
+    const {
+      isLoading,
+      projects,
+      sizeMin,
+      sizeMax,
+      lengthMin,
+      lengthMax,
+      heightMin,
+      heightMax,
+      isEmptyProjects,
+    } = this.state;
     return (
       <div className="projects">
         {isLoading ? <Spinner type="spin" width={150} height={150} color="#ffc000" /> : null}
@@ -62,26 +126,57 @@ class Projects extends React.Component {
                   />
                 ))
               : null}
+            {isEmptyProjects ? <p className="no-projects">لا يوجد مشاريع لعرضها</p> : null}
           </div>
           <div className="projects_filter">
             <p className="projects_filter__title">ترتيب النتائج</p>
             <p>مساحة البناء</p>
             <div className="size-filter">
-              <Input placeholder="الأقل" />
-              <Input placeholder="الأكثر" />
-              <Button label="بحث" />
+              <Input
+                placeholder="الأقل"
+                name="sizeMin"
+                value={sizeMin}
+                onChange={this.handleInputChange}
+              />
+              <Input
+                placeholder="الأكثر"
+                name="sizeMax"
+                value={sizeMax}
+                onChange={this.handleInputChange}
+              />
+              <Button label="بحث" onClick={this.handleSizeFilter} />
             </div>
             <p>طول الواجهة</p>
             <div className="length-filter">
-              <Input placeholder="الأقل" />
-              <Input placeholder="الأكثر" />
-              <Button label="بحث" />
+              <Input
+                placeholder="الأقل"
+                name="lengthMin"
+                value={lengthMin}
+                onChange={this.handleInputChange}
+              />
+              <Input
+                placeholder="الأكثر"
+                name="lengthMax"
+                value={lengthMax}
+                onChange={this.handleInputChange}
+              />
+              <Button label="بحث" onClick={this.handleLengthFilter} />
             </div>
             <p>الإرتفاع</p>
             <div className="hight-filter">
-              <Input placeholder="الأقل" />
-              <Input placeholder="الأكثر" />
-              <Button label="بحث" />
+              <Input
+                placeholder="الأقل"
+                name="heightMin"
+                value={heightMin}
+                onChange={this.handleInputChange}
+              />
+              <Input
+                placeholder="الأكثر"
+                name="heightMax"
+                value={heightMax}
+                onChange={this.handleInputChange}
+              />
+              <Button label="بحث" onClick={this.handleHeightFilter} />
             </div>
             <p className="projects_filter__title">نوع التصميم / المخطط</p>
             <div className="type-filter">
