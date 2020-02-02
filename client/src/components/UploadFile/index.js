6d8343184e414a9b257d53f2bc6bdbf0d27a6b43
unknown <sallamtanna2015@hotmail.com>
2020-02-02 14:45:09 +0200

    change some class component to functional component

--- client/src/components/UploadFile/index.js
+++ client/src/components/UploadFile/index.js
@@ -3,24 +3,20 @@ import { Upload, Button, Icon } from "antd";
 
 import "./style.css";
 
-class UploadFile extends React.Component {
-  state = {};
-
-  render() {
-    const { fileName, handleChange, fileList } = this.props;
-    return (
-      <Upload
-        onChange={handleChange}
-        fileList={fileList}
-        className="upload-file"
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-      >
-        <Button>
-          <Icon type="upload" /> حمل ملف {fileName}
-        </Button>
-      </Upload>
-    );
-  }
-}
+const UploadFile = props => {
+  const { fileName, handleChange, fileList } = props;
+  return (
+    <Upload
+      onChange={handleChange}
+      fileList={fileList}
+      className="upload-file"
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+    >
+      <Button>
+        <Icon type="upload" /> حمل ملف {fileName}
+      </Button>
+    </Upload>
+  );
+};
 
 export default UploadFile;
