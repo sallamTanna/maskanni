c6bbd0d451538ed976fa19f650e0de20aa5e4090
MohammedYehia <m-ya@windowslive.com>
2020-02-08 16:46:18 +0200

    add the basic structure for project page

--- client/src/pages/Project/index.js
+++ client/src/pages/Project/index.js
@@ -1,4 +1,8 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { Component } from "react";
+import { Select, Checkbox, Input } from "antd";
+
+import Button from "../../components/Button";
 import Navbar from "../../components/Navbar";
 
 import stairClimber from "../../assets/stair-climber.svg";
@@ -11,9 +15,12 @@ import placeHolder from "../../assets/place-holder.png";
 
 import "./style.css";
 
+const { Option } = Select;
+const { TextArea } = Input;
+
 function getProjDesc() {
   return (
-    <div className="project-desc">
+    <div className="project-desc line-hy">
       <h3>وصف المشروع</h3>
       <ul>
         <li>
@@ -44,27 +51,20 @@ function generateGallery() {
   return (
     // right main pic
     <>
-      <div className="gallery-sec center shadow">
+      <div className="tall shadow">
         <img src={map} alt="main pic" />
       </div>
-      <div className="gallery-sec gallery-left center">
-        <div className="gallery-left-up center shadow">
-          <img src={map} alt="main pic" className="rotateimg" />
-        </div>
-        <div className="center gallery-left-down">
-          <div className="center shadow gallery-left-down-right">
-            <img src={map} alt="main pic" />
-            {/* right */}
-          </div>
-          <div className="column gallery-left-down-left">
-            <div className="shadow half-hy">
-              <img src={placeHolder} alt="main pic" />
-            </div>
-            <div className="shadow half-hy">
-              <p>عرض المزيد</p>
-            </div>
-          </div>
-        </div>
+      <div className="wide shadow">
+        <img src={map} alt="main pic" className="rotateimg" />
+      </div>
+      <div className="half-tall shadow">
+        <img src={map} alt="main pic" />
+      </div>
+      <div className="shadow ">
+        <img src={placeHolder} alt="main pic" />
+      </div>
+      <div className="shadow">
+        <p>عرض المزيد</p>
       </div>
     </>
   );
@@ -80,6 +80,77 @@ function generateIcon(icon) {
   );
 }
 
+const generateLeftSection = () => {
+  return (
+    <>
+      <div className="space-bt">
+        <div className="column cl-flexstart line-hy">
+          <b>شراء المخطط</b>
+          <p>سعر المخطط يباع بالدولار الأمريكي</p>
+        </div>
+        <p className="f-big3">240$</p>
+      </div>
+      <form className="form">
+        <div>
+          <label htmlFor="selectCopy">حدد طبيعة النسخة</label>
+          <Select
+            id="selectCopy"
+            className="form-select"
+            defaultValue="pdf"
+            onChange={() => console.log(1)}
+          >
+            <Option value="pdf">PDF نسخة واحد الكتروني</Option>
+            <Option value="wdg">WDG نسخة واحد الكتروني</Option>
+          </Select>
+        </div>
+
+        <div className="margin-top-1">
+          <Checkbox onChange={() => console.log(1)}>
+            <b>هل تريد تخصيص هذا المخطط</b>
+          </Checkbox>
+          <p className="margin-1-5-right">إضغط هنا لتفعيل التعديل على المخطط قبل الشراء</p>
+        </div>
+
+        <div>
+          <label htmlFor="selectMore">التعديلات الاضافية</label>
+          <Select
+            id="selectMore"
+            className="form-select"
+            placeholder="حدد التعديلات المطلوبة"
+            disabled
+            onChange={() => console.log(1)}
+          >
+            <Option value="edit">تعديل على المخطط المعماري</Option>
+            <Option value="additional">طلب مخطط اضافي تابع للمخطط المعماري</Option>
+          </Select>
+        </div>
+
+        <div className="margin-top-1">
+          <label htmlFor="msg">رسالة</label>
+          <TextArea id="msg" rows={4} placeholder="أكتب رسالتك للمهندس أو مصمم هذا المخطط" />
+        </div>
+
+        <div className="warn">
+          <div className="edit-time">
+            <p>
+              <span className="warn-msg">فترة السماح بالتعديلات المجانية</span>
+              <br />
+              <span className="mid-font-size">
+                يسمح بالتعديل على المخططات بعد شرائها مجانا لفترة لا تزيد عن أسبوع واحد فقط من تاريخ
+                الشراء.
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <Button block label="شراء المخطط - $240" className="buy-btn" />
+        </div>
+      </form>
+    </>
+  );
+};
+
 class Project extends Component {
   render() {
     return (
@@ -89,30 +160,24 @@ class Project extends Component {
           {/* right section */}
           <div className="right-section">
             <div className="history-tree">الرئيسية -> المشاريع -> شقق عائلية -> شقة رقم A325</div>
+
             <div className="share-section">
               <p>الخطة A325</p>
               <p>
                 مشاركة <span>share</span>
               </p>
             </div>
+
             <div className="shortDetails-section shadow">
               {[design, bed, sofa, stairClimber, carBarking].map(icon => generateIcon(icon))}
             </div>
+
             <div className="gallery">{generateGallery()}</div>
             {getProjDesc()}
             <div className="gallery shadow">Details</div>
           </div>
           {/* left section */}
-          <div className="left-section shadow">
-            <div className="space-bt">
-              <div className="column cl-flexstart">
-                <h3>شراء المخطط</h3>
-                <p>سعر المخطط يباع بالدولار الأمريكي</p>
-              </div>
-              <h1>240 $</h1>
-            </div>
-            <div>sadasd</div>
-          </div>
+          <div className="left-section left-sec-shadow">{generateLeftSection()}</div>
         </div>
       </>
     );
